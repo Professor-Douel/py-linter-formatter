5539f7d89e794071e7de40d8812d0ba4ebdb32ec
Oleksandr Barabanov <imtence@icloud.com>
2024-11-27 16:35:44 +0200

    Formatted

--- app/main.py
+++ app/main.py
@@ -3,18 +3,22 @@ def format_linter_error(error: dict) -> dict:
         "line": error["line_number"],
         "column": error["column_number"],
         "message": error["text"],
-        "name": error["filename"],
-        "source": "flake8"
+        "name": error["code"],
+        "source": "flake8",
     }
     pass
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {file_path: (errors if error else "passed") for error in errors}
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed",
+    }
     pass
-
-
 
 def format_linter_report(linter_report: dict) -> list:
-    return [(path, errors) for path, errors in linter_report.items()]
+    return [
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
+    ]
     pass
-
